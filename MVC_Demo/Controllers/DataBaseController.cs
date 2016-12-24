0c9c75a3472ebd1270b5f25bd8154b3530de6a09
unknown <>
2016-12-24 10:46:06 +0800

    add page jump

--- MVC_Demo/Controllers/DataBaseController.cs
+++ MVC_Demo/Controllers/DataBaseController.cs
@@ -11,15 +11,29 @@ namespace MVC_Demo.Controllers
 {
     public class DataBaseController : Controller
     {
+        //DataBaseHelper helper = new DataBaseHelper(ConfigurationManager.ConnectionStrings["LocalSqlServer"].ToString());
+        private DataBaseHelper helper = new DataBaseHelper(@"Data Source=DESKTOP-130IQ5N;Initial Catalog=Test;Integrated Security=True");
+
         // GET: DataBase
         public ActionResult Index()
         {
-            //DataBaseHelper helper = new DataBaseHelper(ConfigurationManager.ConnectionStrings["LocalSqlServer"].ToString());
-            DataBaseHelper helper = new DataBaseHelper(@"Data Source=DESKTOP-130IQ5N;Initial Catalog=Test;Integrated Security=True");
+            
             List<Table_1> table = new List<Table_1>();
             table = helper.GetAllRecord();
 
             ViewBag.table = table;
+            return View();
+        }
+
+        public ActionResult Update()
+        {
+
+            return View();
+        }
+
+        public ActionResult Delete()
+        {
+
             return View();
         }
     }

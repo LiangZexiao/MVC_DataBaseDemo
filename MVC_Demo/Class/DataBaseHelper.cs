29f96459d932c3bca7d41af04d07a460058bcda1
unknown <>
2016-12-24 09:33:29 +0800

    add functions in dbhelper

--- MVC_Demo/Class/DataBaseHelper.cs
+++ MVC_Demo/Class/DataBaseHelper.cs
@@ -10,21 +10,25 @@ namespace MVC_Demo.Class
 {
     public class DataBaseHelper
     {
-        public string conStr = @"Data Source=DESKTOP-130IQ5N;Initial Catalog=Test;Integrated Security=True";
-        public SqlConnection conn;
+        private SqlConnection conn;
+        private SqlCommand comm;
+        private List<Table_1> result;
 
+       
 
-
-        public List<Table_1> GetAllRecords()
+        public DataBaseHelper(string ConStr)
         {
-            //DataTable result = new DataTable();
-            List<Table_1> result = new List<Table_1>();
-            conn = new SqlConnection(conStr);
+            conn = new SqlConnection(ConStr);
+        }
+
+        public List<Table_1> GetAllRecord()
+        {
             conn.Open();
             string sql = "select * from Table_1";
-
-            SqlCommand comm = new SqlCommand(sql,conn);
+            comm = new SqlCommand(sql, conn);
             SqlDataReader reader = comm.ExecuteReader();
+            result = new List<Table_1>();
+
             while (reader.Read())
             {
                 Table_1 item = new Table_1();
@@ -34,14 +38,60 @@ namespace MVC_Demo.Class
                 result.Add(item);
             }
             reader.Close();
+            conn.Close();
 
-            //SqlDataAdapter ada = new SqlDataAdapter();
-            //ada.SelectCommand = comm;
-            //ada.Fill(result);
+            return result;
+        }
+
+        public int DeleteRecord(int id)
+        {
+            conn.Open();
+            string sql = "delete from Table_1 where id=" + id;
+            comm = new SqlCommand(sql, conn);
+            int result = comm.ExecuteNonQuery();
+            conn.Close();
+
+            return result;
+        }
+        public int AddRecord(Table_1 table)
+        {
+
+            conn.Open();
+            string sql = "insert into Table_1 values('" + table.name + "','" + table.sex + "')";
+            comm = new SqlCommand(sql, conn);
+            int result = comm.ExecuteNonQuery();
 
             conn.Close();
 
+            return result;
+        }
+        public Table_1 SearchRecordByID(int id)
+        {
+            conn.Open();
+            string sql = "select * from Table_1 where(id=" + id + ")";
+            comm = new SqlCommand(sql, conn);
+            SqlDataReader reader = comm.ExecuteReader();
+            Table_1 item = new Table_1();
+            if (reader.Read())
+            {
+                item.id = reader.GetInt32(reader.GetOrdinal("id"));
+                item.name = reader.GetString(reader.GetOrdinal("name"));
+                item.sex = reader.GetString(reader.GetOrdinal("sex"));
+            }
+            reader.Close();
+            conn.Close();
 
+            return item;
+        }
+        public int UpdateRecord(Table_1 record, int id)
+        {
+            string sql = "UPDATE Table_1 SET name = '" + record.name + "',sex='" + record.sex + "' WHERE id = " + Convert.ToInt32(id) + "";
+
+            conn.Open();
+            SqlCommand comm = new SqlCommand(sql, conn);
+            int result = comm.ExecuteNonQuery();
+
+            conn.Close();
             return result;
         }
     }
--- MVC_Demo/Controllers/DataBaseController.cs
+++ MVC_Demo/Controllers/DataBaseController.cs
@@ -5,6 +5,7 @@ using System.Web;
 using System.Web.Mvc;
 using MVC_Demo.Class;
 using MVC_Demo.Models;
+using System.Configuration;
 
 namespace MVC_Demo.Controllers
 {
@@ -13,9 +14,10 @@ namespace MVC_Demo.Controllers
         // GET: DataBase
         public ActionResult Index()
         {
-            DataBaseHelper helper = new DataBaseHelper();
+            //DataBaseHelper helper = new DataBaseHelper(ConfigurationManager.ConnectionStrings["LocalSqlServer"].ToString());
+            DataBaseHelper helper = new DataBaseHelper(@"Data Source=DESKTOP-130IQ5N;Initial Catalog=Test;Integrated Security=True");
             List<Table_1> table = new List<Table_1>();
-            table = helper.GetAllRecords();
+            table = helper.GetAllRecord();
 
             ViewBag.table = table;
             return View();
